4cf026b6690248dfc976802cdeb761a2a6a27d7e
PabloGFC <161745410+PabloGFC@users.noreply.github.com>
2024-12-02 19:42:53 +0100

    Add files via upload

---
+++ holamundo_os/Debug/src/freertos_hello_world.d
@@ -0,0 +1,56 @@
+src/freertos_hello_world.o src/freertos_hello_world.o: \
+ ../src/freertos_hello_world.c \
+ ../../my_base_os0/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/xparameters.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/projdefs.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/portable.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/portmacro.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/xil_types.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/task.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/list.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/queue.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/task.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/timers.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/xil_printf.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/bspconfig.h \
+ ../../my_base_os0/ps7_cortexa9_0/include/xparameters.h
+
+../../my_base_os0/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/xparameters.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/projdefs.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/portable.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/portmacro.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/xil_types.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/task.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/list.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/queue.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/task.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/timers.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/xil_printf.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/bspconfig.h:
+
+../../my_base_os0/ps7_cortexa9_0/include/xparameters.h:
---
+++ holamundo_os/src/freertos_hello_world.c
@@ -0,0 +1,139 @@
+/*
+    Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+    Copyright (C) 2012 - 2018 Xilinx, Inc. All Rights Reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software. If you wish to use our Amazon
+    FreeRTOS name, please do so in a fair use way that does not cause confusion.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    http://www.FreeRTOS.org
+    http://aws.amazon.com/freertos
+
+
+    1 tab == 4 spaces!
+*/
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+/* Xilinx includes. */
+#include "xil_printf.h"
+#include "xparameters.h"
+
+#define TIMER_ID	1
+#define DELAY_10_SECONDS	10000UL
+#define DELAY_1_SECOND		1000UL
+#define TIMER_CHECK_THRESHOLD	9
+/*-----------------------------------------------------------*/
+
+/* The Tx and Rx tasks as described at the top of this file. */
+static void prvTxTask( void *pvParameters );
+static void prvRxTask( void *pvParameters );
+static void vTimerCallback( TimerHandle_t pxTimer );
+/*-----------------------------------------------------------*/
+
+/* The queue used by the Tx and Rx tasks, as described at the top of this
+file. */
+static TaskHandle_t xTxTask;
+static TaskHandle_t xRxTask;
+static QueueHandle_t xQueue = NULL;
+static TimerHandle_t xTimer = NULL;
+char HWstring[15] = "Hello World";
+long RxtaskCntr = 0;
+
+int main( void )
+{
+	const TickType_t x10seconds = pdMS_TO_TICKS(DELAY_10_SECONDS);
+
+	xil_printf("Hello from Freertos example main\r\n");
+	xTaskCreate(prvTxTask, (const char *) "Tx", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &xTxTask );
+
+	xTaskCreate(prvRxTask, (const char *) "GB", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xRxTask );
+
+
+	xQueue = xQueueCreate(1,sizeof(HWstring));
+
+
+	configASSERT(xQueue);
+	xTimer = xTimerCreate( (const char *) "Timer", x10seconds, pdFALSE, (void *) TIMER_ID, vTimerCallback);
+	configASSERT(xTimer);
+
+	xTimerStart(xTimer, 0);
+
+	vTaskStartScheduler();
+
+	for( ;; );
+}
+
+
+/*-----------------------------------------------------------*/
+static void prvTxTask( void *pvParameters )
+{
+const TickType_t x1second = pdMS_TO_TICKS( DELAY_1_SECOND );
+
+	for( ;; )
+	{
+		vTaskDelay(x1second);
+		xQueueSend( xQueue,	HWstring, 0UL);			/* The block time. */
+	}
+}
+
+/*-----------------------------------------------------------*/
+static void prvRxTask(void *pvParameters)
+{
+char Recdstring[15] = "";
+
+	for( ;; )
+	{
+		/* Block to wait for data arriving on the queue. */
+		xQueueReceive(xQueue, Recdstring, portMAX_DELAY );
+
+		/* Print the received data. */
+		xil_printf( "Rx task received string from Tx task: %s\r\n", Recdstring );
+		RxtaskCntr++;
+	}
+}
+
+/*-----------------------------------------------------------*/
+static void vTimerCallback(TimerHandle_t pxTimer)
+{
+	long lTimerId;
+	configASSERT(pxTimer);
+
+	lTimerId = (long) pvTimerGetTimerID(pxTimer);
+
+	if (lTimerId != TIMER_ID) {
+		xil_printf("FreeRTOS Hello World Example FAILED");
+	}
+
+	/* If the RxtaskCntr is updated every time the Rx task is called. The
+	 Rx task is called every time the Tx task sends a message. The Tx task
+	 sends a message every 1 second.
+	 The timer expires after 10 seconds. We expect the RxtaskCntr to at least
+	 have a value of 9 (TIMER_CHECK_THRESHOLD) when the timer expires. */
+	if (RxtaskCntr >= TIMER_CHECK_THRESHOLD) {
+		xil_printf("FreeRTOS Hello World Example PASSED");
+	} else {
+		xil_printf("FreeRTOS Hello World Example FAILED");
+	}
+
+	vTaskDelete( xRxTask );
+	vTaskDelete( xTxTask );
+}
+
